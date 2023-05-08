71eabbe3ae09271f5672ab4a584b9dcdd8b707d7
KimYoSep <SepYoKim6745@github.io>
2023-05-08 15:09:08 +0900

    예제 7-16까지 완료

--- php/chapter07/view8.php
+++ php/chapter07/view8.php
@@ -5,13 +5,13 @@
 </head>
 <body>
     <?php
-        $file_dir = "C:/xampp/htdocs/07/data/";
+        $file_dir = "C:\Users\user\Desktop\kys\php\htdocs\class\php\chapter07/data";
         $file_path = $file_dir.$_FILES["upload"]["name"];
         if(move_uploaded_file($_FILES["upload"]["tmp_name"], $file_path)){
             $img_path = "data/".$_FILES["upload"]["name"];
     ?>
     <ul>
-        <li><img src="<?= $file_path?>"></li>
+        <li><img src="<?= $img_path?>"></li>
         <li><?= $_POST["comment"]?></li>
     </ul>
     <?php
