b4a34fae0a752504d51e999f0bf3d49c9bc7fd80
KimYoSep <SepYoKim6745@github.io>
2023-05-10 16:19:05 +0900

    추가문제 2번

--- php/chapter07/add2.php
+++ php/chapter07/add2.php
@@ -5,20 +5,53 @@
 <body>
     <?php
         $arr = array(11,23,5,71,90,151,133,15,19,9,25,26,24,14,52,30,82,65,47,21);
-        function selectSort(){
+        
+        function arrPrint($arr){
+            echo "[";
+            for($i = 0; $i < 19; $i++)
+            echo $arr[$i].", ";
+            echo $arr[19]."]";
+            echo "<br>";
+        }
 
+        function selectionSort($num){
+            $size = count($num);
+            for($i=0; $i<$size; $i++){
+                $mindx = $i;
+                for($j=i+1; $j<$size; $j++){
+                    if($num[$mindx]>$num[$j]) $mindx = $j;
+                }
+                $temp = $num[$mindx];
+                $num[$mindx]=$a
+            }
         }
         
         function insertSort(){
 
         }
 
-        function bubleSort(){
-
+        function bubleSort($num){
+            $count = 20;
+            for($i=$count-2; $i>=0; $i--){
+                for($j=0; $j<=$i; $j++){
+                    if($num[$j] > $num[$j+1]){
+                        $tmp = $num[$j];
+                        $num[$j] = $num[$j+1];
+                        $num[$j+1] = $tmp;
+                    }
+                }
+                arrPrint($num);
+            }
         }
+        
+
         echo "0) 원본<br>";
         echo "1) 버블 정렬<br>";
-        echo "Before : [{$arr}]";
+        echo "Before : ";
+        arrPrint($arr);
+        bubleSort($arr);
+        echo "After : ";
+        arrPrint($arr);
     ?>
 </body>
 </html>
