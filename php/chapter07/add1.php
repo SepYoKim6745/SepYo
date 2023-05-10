ce4cd11c28b5be8b5f023504f029dbdee52f985c
KimYoSep <SepYoKim6745@github.io>
2023-05-10 10:06:04 +0900

    추가문제1번 해결

---
+++ php/chapter07/add1.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <?php
+        $count = 0;
+        $fp = fopen("lesson6_txt.txt", "r") or die("파일을 열 수 없습니다！");
+        while( !feof($fp) ){
+            $arr = fgets($fp);
+            $arr = preg_replace("/\s+/", "\n", $arr);
+            $count += mb_strlen($arr);
+        }
+        echo "<br>text 글자수 (공백포함) : $count 자";
+        fclose($fp);
+        echo "<br>";
+
+        $count = 0;
+        $fp = fopen("lesson6_txt.txt", "r") or die("파일을 열 수 없습니다！");
+        while( !feof($fp) ){
+            $arr = fgets($fp);
+            $arr = preg_replace("/\s+/", "", $arr);
+            $count += mb_strlen($arr);
+        }
+        echo "<br>text 글자수 (공백제외) : $count 자";
+        fclose($fp);
+    ?>
+</body>
+</html>
---
+++ php/chapter07/add2.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <?php
+        $arr = array(11,23,5,71,90,151,133,15,19,9,25,26,24,14,52,30,82,65,47,21);
+        function selectSort(){
+
+        }
+        
+        function insertSort(){
+
+        }
+
+        function bubleSort(){
+
+        }
+        echo "0) 원본<br>";
+        echo "1) 버블 정렬<br>";
+        echo "Before : [{$arr}]";
+    ?>
+</body>
+</html>
