48447dd5323841bec3de425e6c36c514f6fe3707
kys1479 <gkfhak123@gmail.com>
2023-05-22 00:35:06 +0900

    8장 진행중

---
+++ php/chapter08/counter_2_1.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta charset='utf-8'>
+</head>
+<body>
+    <form name="form1" method="post" action="counter_2_2.php">
+        <input type="button" name="btn1" value="1+ button">
+        <input type="reset" name="reset1" value="Reset">  
+    </form>
+    <?php
+    ?>
+</body>
+</html>
