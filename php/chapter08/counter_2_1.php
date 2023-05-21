358dd461c90e64fbb00f24b905d70699595d7108
KimYoSep <gkfhak123@gmail.com>
2023-05-22 01:25:17 +0900

    예제1번 거의 완성?

--- php/chapter08/counter_2_1.php
+++ php/chapter08/counter_2_1.php
@@ -3,11 +3,28 @@
     <meta charset='utf-8'>
 </head>
 <body>
-    <form name="form1" method="post" action="counter_2_2.php">
-        <input type="button" name="btn1" value="1+ button">
-        <input type="reset" name="reset1" value="Reset">  
-    </form>
     <?php
+        session_start();
+
+        if (!isset($_SESSION['cnt'])) {
+            // If not, initialize it to 0
+            $_SESSION['cnt'] = 0;
+        }
+        
+        // Increment the count value if the increment button is clicked
+        if (isset($_POST['btn1'])) {
+            $_SESSION['cnt']++;
+        }
+        
+        // Reset the count value to 0 if the reset button is clicked
+        if (isset($_POST['reset1'])) {
+            $_SESSION['cnt'] = 0;
+        }
     ?>
+    <form name="form1" method="post" action="counter_2_1.php">
+        <input type="submit" name="btn1" value="+1 button">
+        <input type="submit" name="reset1" value="Reset"><br>
+        <?php echo $_SESSION['cnt']; ?>
+    </form>
 </body>
 </html>
