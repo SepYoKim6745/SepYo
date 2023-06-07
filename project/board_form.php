6c86189bec9462eeef02f160a7caf52d8a86ad93
KimYoSep <gkfhak123@gmail.com>
2023-06-07 20:30:00 +0900

    board_list.php 작성중

---
+++ project/board_form.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>PHP 프로그래밍 입문</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link rel="stylesheet" type="text/css" href="./css/board_form_style.css">
+    <script>
+        function check_input() {
+            if (!document.board_form.subject.value) {
+                alert("제목을 입력하세요!");
+                document.board_form.subject.focus();
+                return;
+            }
+            if (!document.board_form.content.value) {
+                alert("내용을 입력하세요!");
+                document.board_form.content.focus();
+                return;
+            }
+            document.board_form.submit();
+        }
+    </script>
+</head>
+
+<body>
+    <header>
+        <?php include "header.php"; ?>
+    </header>
+    <section>
+        <br>
+        <div id="board_box" class="container">
+            <h3 id="board_title">
+                게시판 > 글 쓰기
+            </h3>
+            <hr>
+            <form name="board_form" method="post" action="board_insert.php" enctype="multipart/form-data">
+                <ul id="board_form">
+                    <li>
+                        <span class="col1">이름 : </span>
+                        <span class="col2">
+                            <?= $username ?>
+                        </span>
+                    </li>
+                    <hr>
+                    <li>
+                        <div class="row">
+                            <label for="subject" class="col-1 col-form-label">제 목 </label>
+                            <div class="col-11">
+                                <input class="form-control" name="subject" type="text">
+                            </div>
+                        </div>
+                    </li>
+                    <hr>
+                    <li id="text_area">
+                        <div class="row">
+                            <label for="content" class="col-1 col-form-label">내용 </label>
+                            <div class="col-11">
+                                <textarea name="content" type="textarea" class="form-control" rows="10"></textarea>
+                            </div>
+                        </div>
+                    </li>
+                    <hr>
+                    <li>
+                        <div class="row">
+                            <label for="file-label" class="col-1 col-form-label">첨부파일</label>
+                            <div class="col-10">
+                                <span class="col-12" style="float:right;"><input class="form-control" type="file" name="upfile"></span>
+                            </div>
+                        </div>
+
+                    </li>
+                </ul>
+                <ul class="buttons">
+                    <li><button class="btn btn-primary" type="button" onclick="check_input()">완료</button></li>
+                    <li><button class="btn btn-primary" type="button" onclick="location.href='board_list.php'">목록</button></li>
+                </ul>
+            </form>
+        </div> <!-- board_box -->
+    </section>
+    <footer>
+        <?php include "footer.php"; ?>
+    </footer>
+</body>
+
+</html>
--- project/message_response_form.php
+++ project/message_response_form.php
@@ -1,93 +1,109 @@
 <!DOCTYPE html>
 <html>
-<head> 
-<meta charset="utf-8">
-<title>PHP 프로그래밍 입문</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="./css/common.css">
-<link rel="stylesheet" type="text/css" href="./css/message.css">
-<script>
-  function check_input() {
-      if (!document.message_form.subject.value)
-      {
-          alert("제목을 입력하세요!");
-          document.message_form.subject.focus();
-          return;
-      }
-      if (!document.message_form.content.value)
-      {
-          alert("내용을 입력하세요!");    
-          document.message_form.content.focus();
-          return;
-      }
-      document.message_form.submit();
-   }
-</script>
+
+<head>
+	<meta charset="utf-8">
+	<title>PHP 프로그래밍 입문</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="./css/message.css">
+	<script>
+		function check_input() {
+			if (!document.message_form.subject.value) {
+				alert("제목을 입력하세요!");
+				document.message_form.subject.focus();
+				return;
+			}
+			if (!document.message_form.content.value) {
+				alert("내용을 입력하세요!");
+				document.message_form.content.focus();
+				return;
+			}
+			document.message_form.submit();
+		}
+	</script>
 </head>
-<body> 
-    <?php include "header.php";?>
-<section>
-	<div id="main_img_bar">
-        <img src="./img/main_img.png">
-    </div>
-   	<div id="message_box">
-	    <h3 id="write_title">
-	    		답변 쪽지 보내기
-		</h3>
-<?php
-	$num  = $_GET["num"];
 
-	$con = mysqli_connect("localhost", "user1", "12345", "project");
-	$sql = "select * from message where num=$num";
-	$result = mysqli_query($con, $sql);
+<body>
+	<?php include "header.php"; ?>
+	<section>
+		<br>
+		<div id="message_box" class="container">
+			<h3 id="title">
+				답변 쪽지 보내기
+			</h3>
+			<hr>
+			<?php
+			$num = $_GET["num"];
 
-	$row = mysqli_fetch_array($result);
-	$send_id      = $row["send_id"];
-	$rv_id      = $row["rv_id"];
-	$subject    = $row["subject"];
-	$content    = $row["content"];
+			$con = mysqli_connect("localhost", "user1", "12345", "project");
+			$sql = "select * from message where num=$num";
+			$result = mysqli_query($con, $sql);
 
-	$subject = "RE: ".$subject; 
+			$row = mysqli_fetch_array($result);
+			$send_id = $row["send_id"];
+			$rv_id = $row["rv_id"];
+			$subject = $row["subject"];
+			$content = $row["content"];
 
-	$content = "> ".$content; 
-	$content = str_replace("\n", "\n>", $content);
-	$content = "\n\n\n-----------------------------------------------\n".$content;
+			$subject = "RE: " . $subject;
 
-	$result2 = mysqli_query($con, "select name from members where id='$send_id'");
-	$record = mysqli_fetch_array($result2);
-	$send_name    = $record["name"];
-?>		
-	    <form  name="message_form" method="post" action="message_insert.php?send_id=<?=$userid?>">
-	    	<input type="hidden" name="rv_id" value="<?=$send_id?>">
-	    	<div id="write_msg">
-	    	    <ul>
-				<li>
-					<span class="col1">보내는 사람 : </span>
-					<span class="col2"><?=$userid?></span>
-				</li>	
-				<li>
-					<span class="col1">수신 아이디 : </span>
-					<span class="col2"><?=$send_name?>(<?=$send_id?>)</span>
-				</li>	
-	    		<li>
-	    			<span class="col1">제목 : </span>
-	    			<span class="col2"><input name="subject" type="text" value="<?=$subject?>"></span>
-	    		</li>	    	
-	    		<li id="text_area">	
-	    			<span class="col1">글 내용 : </span>
-	    			<span class="col2">
-	    				<textarea name="content"><?=$content?></textarea>
-	    			</span>
-	    		</li>
-	    	    </ul>
-	    	    <button type="button" onclick="check_input()">보내기</button>
-	    	</div>
-	    </form>
-	</div> <!-- message_box -->
-</section> 
-<footer>
-    <?php include "footer.php";?>
-</footer>
+			$content = "> " . $content;
+			$content = str_replace("\n", "\n>", $content);
+			$content = "\n\n\n-----------------------------------------------\n" . $content;
+
+			$result2 = mysqli_query($con, "select name from members where id='$send_id'");
+			$record = mysqli_fetch_array($result2);
+			$send_name = $record["name"];
+			?>
+			<form name="message_form" method="post" action="message_insert.php?send_id=<?= $userid ?>">
+				<input type="hidden" name="rv_id" value="<?= $send_id ?>">
+				<div id="write_msg">
+					<ul>
+						<li>
+							<span class="col1">보내는 사람 : </span>
+							<span class="col2">
+								<?= $userid ?>
+							</span>
+						</li>
+						<hr>
+						<li>
+							<span class="col1">수신 아이디 : </span>
+							<span class="col2">
+								<?= $send_name ?>(
+								<?= $send_id ?>)
+							</span>
+						</li>
+						<hr>
+						<li>
+							<div class="row">
+								<label for="content" class="col-1 col-form-label">내용 </label>
+								<div class="col-11">
+									<textarea name="content" type="textarea" class="form-control" rows="10"></textarea>
+								</div>
+							</div>
+						</li>
+						<hr>
+						<li id="text_area">
+							<div class="row">
+								<label for="content" class="col-1 col-form-label">내용 </label>
+								<div class="col-11">
+									<textarea name="content" type="textarea" class="form-control"
+										rows="10"><?= $content ?></textarea>
+								</div>
+							</div>
+						</li>
+					</ul>
+					<button type="button" class="btn btn-primary" style="float:right;"
+						onclick="check_input()">보내기</button>
+				</div>
+			</form>
+		</div> <!-- message_box -->
+	</section>
+	<br>
+	<footer>
+		<?php include "footer.php"; ?>
+	</footer>
 </body>
+
 </html>
